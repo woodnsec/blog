f4892136304fd3c701c2ab9f1d6aca7a1bd301b9
Nate Wood <nwood01@unomaha.edu>
2019-05-21 13:01:57 -0500

    updated icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
         background_color: `#292a2d`,
         theme_color: `#292a2d`,
         display: `minimal-ui`,
-        icon: `src/images/hello-icon.png`,
+        icon: `src/images/n.png`,
       },
     },
   ],
